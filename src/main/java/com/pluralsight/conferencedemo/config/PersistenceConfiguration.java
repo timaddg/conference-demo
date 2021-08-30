92e8cc637c2fb418fd07b0dfa72e3d9907fbbba2
timaddg <manunited.rooney10@gmail.com>
2021-08-30 11:27:07 +0530

    First

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,26 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@ConfigurationProperties(prefix = "spring.datasource")
+public class PersistenceConfiguration {
+    @Bean
+    @Primary
+    public DataSource dataSource(){
+
+        return DataSourceBuilder.create().url("").username("").password("").build();
+        /*builder.url("jdbc:mysql://localhost:3306/conference_demo");
+        builder.username("root");
+        builder.password("Manchester83!");
+        System.out.println("Bean has been initialised");
+
+        return builder.build();
+         */
+}
+}
---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -0,0 +1,45 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+
+public class SpeakerController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
+    }
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+    @PostMapping
+    // @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+    @RequestMapping(value = "{id}", method=RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        speakerRepository.deleteById(id);
+    }
+    @RequestMapping(value="{id}",method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id,@RequestBody Speaker speaker){
+        Speaker existingSession =speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker,existingSession,"speaker_id");
+        return speakerRepository.saveAndFlush(existingSession);
+    }
+
+}
